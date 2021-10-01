9b96908bb861d7cb56d2f77e05e1a5b2a9458a94
unknown <raghu.mce2000@gmail.com>
2021-10-01 13:32:16 +0530

    file completed

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
-from matplotlib.colors import ListedColorma
+from matplotlib.colors import ListedColormap
 import os
 
 def prepare_data(df):
